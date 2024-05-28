50f5607b3dcc2dc008f94b5860c10447632d0e03
Saurabh Pingale <saurabhpingale93@gmail.com>
2024-05-28 12:46:35 +0530

    Create a server.js for handling server setup

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import identifyRouter from './routes/identify'
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 const app = express();
 
@@ -8,7 +11,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-
+app.use('/identify', identifyRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server runnning on port ${process.env.PORT}`);
